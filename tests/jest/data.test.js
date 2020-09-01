2280ea44a3a61ddb1d360161a834b726f2014d42
Hugo <HugoDF@users.noreply.github.com>
2020-09-01 16:55:57 +0100

    fix jest component .$data issue (#49)
    * fix jest component. issue
    
    * fix lint

---
+++ tests/jest/data.test.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-undef */
+const {render} = require('../../src/main');
+
+test('render - sets $data properties on the component', () => {
+  const component = render(
+    `<div x-data="{ foo: 'bar' }">
+  <span x-text="foo"></span>
+</div>`
+  );
+  expect(component.$data.foo).toEqual('bar');
+});
+
+test('render - updating $data works', async () => {
+  const component = render(
+    `<div x-data="{ foo: 'bar' }">
+  <span x-text="foo"></span>
+</div>`
+  );
+
+  component.$data.foo = 'baz';
+
+  await component.$nextTick();
+  expect(component.$data.foo).toEqual('baz');
+});
