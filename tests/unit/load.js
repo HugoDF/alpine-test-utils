15d096048884c6874a968f271ec6da9816fde6d2
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2020-09-01 17:43:58 +0100

    chore(use-uvu-for-unit-tests)

--- tests/unit/load.js
+++ tests/unit/load.js
@@ -1,25 +1,26 @@
-import test from 'ava';
+import {test} from 'uvu';
+import * as assert from 'uvu/assert';
 import path from 'path';
 import {load, loadSync} from '../../src/main';
 
-test('load - file with multiple components', async (t) => {
+test('load - file with multiple components', async () => {
   const components = await load(
     path.join(__dirname, '../fixtures/multiple-components.html')
   );
-  t.is(components.length, 3);
-  t.is(components[0], `<div x-data="" name="component-1"></div>`);
-  t.is(components[1], `<div x-data="" name="component-2"></div>`);
-  t.is(components[2], `<div x-data="" name="component-3"></div>`);
+  assert.is(components.length, 3);
+  assert.is(components[0], `<div x-data="" name="component-1"></div>`);
+  assert.is(components[1], `<div x-data="" name="component-2"></div>`);
+  assert.is(components[2], `<div x-data="" name="component-3"></div>`);
 });
 
-test('load - file with single component', async (t) => {
+test('load - file with single component', async () => {
   const component = await load(
     path.join(__dirname, '../fixtures/single-component.html')
   );
-  t.is(component, `<div x-data="" name="component-1"></div>`);
+  assert.is(component, `<div x-data="" name="component-1"></div>`);
 });
 
-test('loadSync - file with multiple components', (t) => {
+test('loadSync - file with multiple components', () => {
   const calls = [];
   const realWarn = console.warn;
   console.warn = (...args) => {
@@ -29,12 +30,12 @@ test('loadSync - file with multiple components', (t) => {
   const components = loadSync(
     path.join(__dirname, '../fixtures/multiple-components.html')
   );
-  t.is(components.length, 3);
-  t.is(components[0], `<div x-data="" name="component-1"></div>`);
-  t.is(components[1], `<div x-data="" name="component-2"></div>`);
-  t.is(components[2], `<div x-data="" name="component-3"></div>`);
+  assert.is(components.length, 3);
+  assert.is(components[0], `<div x-data="" name="component-1"></div>`);
+  assert.is(components[1], `<div x-data="" name="component-2"></div>`);
+  assert.is(components[2], `<div x-data="" name="component-3"></div>`);
   // Check warning
-  t.deepEqual(calls, [
+  assert.equal(calls, [
     [
       'alpine-test-utils: loadSync() can cause performance issues, prefer async "load()"'
     ]
@@ -42,7 +43,7 @@ test('loadSync - file with multiple components', (t) => {
   console.warn = realWarn;
 });
 
-test('loadSync - file with single component', (t) => {
+test('loadSync - file with single component', () => {
   const calls = [];
   const realWarn = console.warn;
   console.warn = (...args) => {
@@ -52,12 +53,14 @@ test('loadSync - file with single component', (t) => {
   const component = loadSync(
     path.join(__dirname, '../fixtures/single-component.html')
   );
-  t.is(component, `<div x-data="" name="component-1"></div>`);
+  assert.is(component, `<div x-data="" name="component-1"></div>`);
   // Check warning
-  t.deepEqual(calls, [
+  assert.equal(calls, [
     [
       'alpine-test-utils: loadSync() can cause performance issues, prefer async "load()"'
     ]
   ]);
   console.warn = realWarn;
 });
+
+test.run();
